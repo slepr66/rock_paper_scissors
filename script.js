43601dbff56a9c1f3f511837ccbf8b3cafcf3a76
slepr66 <shinaniyash@gamil.com>
2025-01-30 13:25:17 +0500

    added a function to get a random choice from computer

---
+++ script.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+    let choiceNum = Math.random();
+    let choice;
+    if (choiceNum <= 0.334) {
+       choice = "rock";
+    } else if (choiceNum > 0.333 && choiceNum <= 0.667) {
+        choice = "paper";
+    } else {
+        choice = "scissors"
+    }
+
+    return choice;
+};
+
+let computerChoice = getComputerChoice()
